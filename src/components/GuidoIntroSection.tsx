7bc534e9198fbf7090340f2464770eaf3f5ebfa6
[dyad] <git@dyad.sh>
2025-11-18 21:11:39 +0100

    [dyad] wrote 1 file(s)

--- src/components/GuidoIntroSection.tsx
+++ src/components/GuidoIntroSection.tsx
@@ -33,7 +33,7 @@ const GuidoIntroSection = () => {
         />
         {/* Static bird near the image, adjusted to sit exactly in the middle on top */}
         <FlyingBirdIllustration
-          className="absolute -top-[79px] left-[43%] -translate-x-1/2" // Aangepast naar -top-[79px] (was -top-20, wat -80px is)
+          className="absolute -top-12 md:-top-16 lg:-top-20 left-1/2 -translate-x-1/2" // Aangepast voor responsieve top en centrering
           size="small"
           initialX="0%"
           initialY="0%"
